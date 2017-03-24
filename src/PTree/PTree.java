361d9c622c503fb60b0cdbad131311f5e1fd3e90
Jonathan <jon_mcdevitt@hotmail.com>
2017-03-24 10:48:58 -0300

    Setting up preliminary tree and node objects.

---
+++ src/PTree/PTree.java
@@ -0,0 +1,8 @@
+package PTree;
+
+/**
+ * Created by Jonathan McDevitt on 2017-03-24.
+ */
+public class PTree {
+
+}
---
+++ src/TTree/TNode.java
@@ -0,0 +1,8 @@
+package TTree;
+
+/**
+ * Created by Jonathan McDevitt on 2017-03-24.
+ */
+public class TNode {
+
+}
